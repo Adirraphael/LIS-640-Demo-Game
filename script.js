7e994e7508075d4cf09890596a1022d6025d1a82
Adir Raphael <araphael2@wisc.edu>
2024-11-03 20:52:50 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+
+function jump() {
+    if (character.classList != "animate") {
+        character.classList.add("animate");
+    }
+    setTimeout(function() {
+        character.classList.remove("animate");
+    }, 700); // Increased jump duration for higher jump
+}
+
+var checkDead = setInterval(function() {
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    
+    if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("Game Over. Try Again!");
+    }
+}, 10);
+
+function restartGame() {
+    // Reset block position and animation
+    block.style.display = "block";
+    block.style.animation = "block 1s infinite linear";
+    alert("Game Restarted!");
+}
